13ac4b4a4376f97b167084236036a3d45d89aabd
Ussf-nassrallah <nassrallah.dev@gmail.com>
2023-09-18 11:36:14 +0100

    FileStorage: updated

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -65,6 +65,8 @@ class FileStorage:
         delete obj from __objects if it’s inside
         if obj is equal to None, the method should not do anything
         """
-        if obj is not None:
-            del obj
-            self.save()
+        if obj is None:
+            return
+        objKey = obj.__class__.__name__ + "." + obj.id
+        del self.__objects[objKey]
+        self.save()
