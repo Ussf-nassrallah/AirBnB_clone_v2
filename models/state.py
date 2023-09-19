c0bfcfb1f043f3d2144af2e07a0d7286d74f6668
Redwan Ben Yechou <67595559+Riyo3350G@users.noreply.github.com>
2023-09-19 22:37:07 +0100

    Update state.py

--- models/state.py
+++ models/state.py
@@ -12,7 +12,7 @@ class State(BaseModel, Base):
     name = Column(String(128), nullable=False)
 
     if getenv("HBNB_TYPE_STORAGE") == "db":
-        cities = relationship('City', backref='state', cascade='all, delete')
+        cities = relationship('City', backref='state', cascade='delete')
     else:
         @property
         def cities(self):
