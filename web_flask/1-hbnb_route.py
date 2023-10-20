755dfba41fa828c4950469251385fe3128230ce7
Ussf-nassrallah <nassrallah.dev@gmail.com>
2023-10-20 03:14:23 -0700

    /hbnb: display 'HBNB'

--- web_flask/1-hbnb_route.py
+++ web_flask/1-hbnb_route.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
 """ script that starts a Flask web application """
-from flask import FLASK
+from flask import Flask
 
-app = FLASK(__name__)
+app = Flask(__name__)
 
 
 @app.route("/", strict_slashes=False)
